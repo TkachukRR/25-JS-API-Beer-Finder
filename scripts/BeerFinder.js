aeee2505646087e8833f2564c8755fedf71974c1
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-13 13:39:40 +0100

    #001 Added header searc form and styles

--- scripts/BeerFinder.js
+++ scripts/BeerFinder.js
@@ -1,7 +1,13 @@
-import { APP_NAME, HEADER_BTN_FAVOURITES } from "./constants.js";
+import {
+  APP_NAME,
+  HEADER_BTN_FAVOURITES,
+  SERCH_FORM_ICON,
+  SERCH_FORM_PLACEHOLDER,
+} from "./constants.js";
 
 export class BeerFinder {
   #appTag;
+  #lastSearches = ["1", "2", "3"];
 
   constructor() {
     this.#appTag = document.querySelector("#beerFinder");
@@ -18,17 +24,36 @@ export class BeerFinder {
   makeHeaderMarkup() {
     return `
     <header class="header">
-        ${this.makeHeaderTitle()}
-        ${this.makeButton(HEADER_BTN_FAVOURITES)}
+        ${this.makeHeaderTitleMarkup()}
+        ${this.makeButtonMarkup(HEADER_BTN_FAVOURITES)}
+        ${this.makeSearcFormhMarkup()}
     </header>
     `;
   }
 
-  makeHeaderTitle() {
+  makeHeaderTitleMarkup() {
     return `<h1 class="header__title">${APP_NAME.toUpperCase()}</h1>`;
   }
 
-  makeButton(btnName) {
+  makeButtonMarkup(btnName) {
     return `<button type="button" class="btn button__${btnName.toLowerCase()}">${btnName}</button>`;
+  }
+
+  makeSearcFormhMarkup() {
+    return `
+     <form class="search">
+        <label class="search__lable">
+            <input type="text" name="search" class="search__input" placeholder="${SERCH_FORM_PLACEHOLDER}"/>
+        </label>
+        <button type="submit" class="search__button" >${SERCH_FORM_ICON}</button>
+        <ul class="search__list">
+            ${this.makeListItemsMarkupFromArray(this.#lastSearches)}
+        </ul>
+    </form>
+    `;
+  }
+
+  makeListItemsMarkupFromArray(array) {
+    return array.map((item) => `<li>${item}</li>`).join("");
   }
 }
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,2 +1,4 @@
 export const APP_NAME = "Bear Finder";
 export const HEADER_BTN_FAVOURITES = "Favourites";
+export const SERCH_FORM_PLACEHOLDER = "serch...";
+export const SERCH_FORM_ICON = "&#128269";
