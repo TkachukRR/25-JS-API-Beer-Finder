e8fe80638e0b840d2f35cbbd75be3fc6131caf05
Roman Tkachuk <tkachukrr@gmail.com>
2023-02-13 12:20:27 +0100

    #001 Added title to page header

--- scripts/BeerFinder.js
+++ scripts/BeerFinder.js
@@ -1,3 +1,29 @@
+import { APP_NAME } from "./constants.js";
+
 export class BeerFinder {
-  constructor() {}
+  #appTag;
+
+  constructor() {
+    this.#appTag = document.querySelector("#beerFinder");
+
+    this.renderHeader();
+  }
+
+  renderHeader() {
+    const headeMarkup = this.makeHeadeMarkup();
+
+    this.#appTag.insertAdjacentHTML("beforeend", headeMarkup);
+  }
+
+  makeHeadeMarkup() {
+    return `
+    <header class="header">
+        ${this.makeHeaderTitle()}
+    </header>
+    `;
+  }
+
+  makeHeaderTitle() {
+    return `<h1 class="header__title">${APP_NAME}</h1>`;
+  }
 }
